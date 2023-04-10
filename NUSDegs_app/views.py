61d1462fa1b9861bdc56ec9f49ad24444508df32
nuovonatura <nuovonatura@outlook.com>
2023-04-11 04:12:15 +0800

    Basic implementation of api.

--- NUSDegs_app/views.py
+++ NUSDegs_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from NUSDegs_app.main import modsplanner
+
 
 # Create your views here.
+
+# To handle reques from the frontend
+
+def get_plans_view(request):
+    return JsonResponse(modsplanner(request));
